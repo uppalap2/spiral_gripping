45b58409964d7d9a3192c48d0f19d78b260108e7
uppalap2 <uppalap2@illinois.edu>
2017-03-21 22:37:23 -0500

    With bias angle(have to be checked)

--- Materialmodel/preForceAnalysis.m
+++ Materialmodel/preForceAnalysis.m
@@ -6,7 +6,7 @@
 % >=L_min then add a 1 on the next row.
 function preForceAnalysis
 clear all % needed 
-L = 26e-2; %%%%%%%CHANGE THIS
+L = 44e-2; %%%%%%%CHANGE THIS
 
 Pressure = 8:18;
 result_7088_26 = zeros(length(Pressure),5);
@@ -18,9 +18,13 @@ for i = 1:length(Pressure)
     
     initial_shape = getShapeGravity(Pressure(i), alpha , beta,fitmatFile,L);
     
-    TangVec = angle_detect(initial_shape);
-    CylCurv = initial_shape(end,13)/(sind(TangVec(end)))^2;
-    MinLength = 2.5*pi*CylCurv^(-1)/sind(mean(TangVec));
+    plot3(initial_shape(:,1),initial_shape(:,2),initial_shape(:,3),'r');
+    axis equal
+    grid on
+    
+    [TangVec, bias_angle] = angle_detect(initial_shape);
+    CylCurv = initial_shape(end,13)/(sind(TangVec(end)-bias_angle))^2;
+    MinLength = 2.5*pi*CylCurv^(-1)/sind(mean(TangVec)-bias_angle);
     check = MinLength>=L;
     
     result_7088_26(i,:) = [L Pressure(i) CylCurv^(-1) MinLength check];%%%%%%%%CHANGE THIS
@@ -35,7 +39,7 @@ end
 
 
 
-function TangVec = angle_detect(a)
+function [TangVec, bias_angle] = angle_detect(a)
 CentralAxis = [0 0 1];
 
 for i = 1:length(a)
@@ -43,5 +47,6 @@ for i = 1:length(a)
     R(:,:,i) = reshape(a(i,4:12),3,3); % this should be transposed default is column wise
     TangVec(i) = atan2d(norm(cross(R(3,:,i)',CentralAxis)),dot(R(3,:,i)',CentralAxis));
 end
-
+bias_angle = atan2d(norm(cross([mean(a(:,1));mean(a(:,2));mean(a(:,3))],CentralAxis)),...
+    dot([mean(a(:,1));mean(a(:,2));mean(a(:,3))],CentralAxis));
 end
