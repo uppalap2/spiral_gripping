b729a17ea1f82ab389f7b76a051b04792a62642c
uppalap2 <uppalap2@illinois.edu>
2017-03-29 10:44:35 -0500

    changed sign of delta and Length

--- Materialmodel/main_1.m
+++ Materialmodel/main_1.m
@@ -1,4 +1,4 @@
-clear all%
+ clear all
  clc
 
 
@@ -7,21 +7,21 @@ pressure   = 18; % in psi
 
 alpha = 85*pi/180; beta = 58*pi/180;
 
-option = 1; % 1 for shape with gravity and 2 for grasping around a object
+option = 2; % 1 for shape with gravity and 2 for grasping around a object
 
-L          = 45e-2;
+L          = 55e-2;
 gravity_on = 1;
 n_t_1      = 61;%51 for < 20 % 61 for 20
-n_f        = 61;%31 for < 20 % 61 for 20
+n_f        = 41;%31 for < 20 % 61 for 20
 % r_cyl      =  1.3e-2+.0048;
-r_cyl      =  1.3e-2;
+r_cyl      =  .014;
 Ro = 13/64*.0254;
 Ri = 3/16*.0254;
 
 
-load Material_fit_5088.mat
-Eb = 6*.6029e6; % 50 88  (.6134) for 60 88 (.4386) for 70,88
-
+load Material_fit_6088.mat
+Eb = 7e5; % 50 88  (.6134) for 60 88 (.4386) for 70,88
+WpL = .0326; % .0332-50, .0326-60, .0345-70
 lambda1 = interp1(Material_fit(:,1),Material_fit(:,2),pressure);
 delta = L.*Ro.^(-1).*cos(alpha).^3.*cos(beta).^3.*csc(alpha+(-1).*beta) ...
   .^2.*csc(alpha+beta).*(((-1).*lambda1.^2+sec(beta).^2).*tan(alpha) ...
@@ -32,13 +32,13 @@ delta = L.*Ro.^(-1).*cos(alpha).^3.*cos(beta).^3.*csc(alpha+(-1).*beta) ...
   -1).*lambda1.^2+sec(alpha).^2).*tan(beta).^2);
 
 curvature = (lambda1-1)/(2*Ro);
-torsion = delta/L;
+torsion = -delta/L;
 
 
 tic
                      
 [a,b,initial_shape,d,e]  = getShape(curvature,... % e only for cons3 function
-                       torsion,Eb,...
+                       torsion,Eb,WpL,...
                        L,...
                        gravity_on,...
                        n_t_1,...
@@ -47,7 +47,11 @@ tic
                    
 toc
 
-plot3(initial_shape(:,1),initial_shape(:,2),initial_shape(:,3),'r')
+if option == 1
+    plot3(initial_shape(:,1),initial_shape(:,2),initial_shape(:,3),'r')
+else
+    plot3(a(:,1),a(:,2),a(:,3),'r')
+end
 grid on
  axis equal
  xlabel 'X'
